959211c2910a91c199ee2283fc30eb95b9c688b8
allancassio <allan.cassio1@gmail.com>
2023-01-30 08:39:26 -0300

    init project

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <div>
+      <div className="card">
+        <div>navegação</div>
+        <div>conteudo</div>
+      </div>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { App } from "./app";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <h1>hello, Allan Cássio</h1>
+    <App />
   </React.StrictMode>,
 );
